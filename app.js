955cc3eeae601a00b4947649182145a408fe0ef2
visheshtomarr <visheshtomar7800@gmail.com>
2025-03-07 22:41:18 +0530

    Created get api for all tours

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
-// Creating a 'get' request.
-app.get('/', (req, res) => {
+// // Creating a 'get' request.
+// app.get('/', (req, res) => {
+//     res
+//         .status(200)
+//         .json({ message: 'Hello from the server', app: 'Natours' });
+// });
+
+// // Creating a 'post' request.
+// app.post('/', (req, res) => {
+//     res.send('You can post to this endpoint!');
+// })
+
+// Getting 'tours' data from file.
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+// Creating 'get' api for getting all tours.
+app.get('/api/v1/tours', (req, res) => {
     res
         .status(200)
-        .json({ message: 'Hello from the server', app: 'Natours' });
+        .json({
+            status: 'success',
+            results: tours.length,
+            data: {
+                tours
+            },
+        });
 });
-
-// Creating a 'post' request.
-app.post('/', (req, res) => {
-    res.send('You can post to this endpoint!');
-})
 
 const port = 8000;
 app.listen(port, () => {
