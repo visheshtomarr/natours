fbd1477f39a115734c512d8fa1f8fb8e7dd23ef7
visheshtomarr <visheshtomar7800@gmail.com>
2025-03-08 14:54:54 +0530

    Created get api for a single tour

--- app.js
+++ app.js
@@ -36,7 +36,37 @@ app.get('/api/v1/tours', (req, res) => {
         });
 });
 
-// Creating a 'post' request to create a new tour.
+// Creating a 'get' api to get a single tour by id.
+// Using ":" creates a new parameter variable for us. 
+app.get('/api/v1/tours/:id', (req, res) => {
+    // console.log(req.params);
+    // Convert the value of id from the params to a number type.
+    const id = Number(req.params.id);
+
+    // Find the tour corresponding to the id.
+    const tour = tours.find(tour => tour.id === id);
+
+    // If no tour with the provided id is found, we return "404 not found".
+    if (!tour) {
+        res
+            .status(404)
+            .json({
+                status: "fail",
+                message: "Invalid ID",
+            });
+    }
+
+    res
+        .status(200)
+        .json({
+            status: 'success',
+            data: {
+                tour
+            },
+        });
+});
+
+// Creating a 'post' api to create a new tour.
 app.post('/api/v1/tours', (req, res) => {
     // console.log(req.body);
 
