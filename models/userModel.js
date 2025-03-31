4b70dfe8bea6d55faab2615d05e06fd80f70289a
visheshtomarr <visheshtomar7800@gmail.com>
2025-03-31 13:26:04 +0530

    Validate and encrypt user's password

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 // Create a user schema for our document.
 const userSchema = new mongoose.Schema({
@@ -23,8 +24,30 @@ const userSchema = new mongoose.Schema({
     },
     passwordConfirm: {
         type: String,
-        required: [true, 'A user needs to confirm their password']
+        required: [true, 'A user needs to confirm their password'],
+        validate: {
+            // This only works on "SAVE" and "CREATE" and not when a
+            // password is updated.
+            validator: function (value) {
+                return value === this.password;
+            },
+            message: 'Passwords does not match!'
+        }
     }
+});
+
+// Hash user's password just before saving it into the database.
+userSchema.pre('save', async function (next) {
+    // Run this middleware if the only the "password" field is modified.
+    if (!this.isModified('password')) return next();
+
+    // Hash the password with the cost parameter of 12.
+    this.password = await bcrypt.hash(this.password, 12);
+
+    // Only need this field to validate the entered password. 
+    // So we will not store 'passwordConfirm' field in DB.
+    this.passwordConfirm = undefined;
+    next();
 });
 
 // Creates a model from our user schema.
