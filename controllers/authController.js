00e3d7792144bffaed81f9d1d0e40d6ed21a5a47
visheshtomarr <visheshtomar7800@gmail.com>
2025-04-03 21:48:43 +0530

    Refactored code to send jwt via cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,26 +14,30 @@ const signToken = id => {
 
 const createAndSendToken = (user, statusCode, res) => {
     const token = signToken(user._id);
+    const cookieOptions = {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
+        httpOnly: true
+    };
+
+    if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+    res.cookie('jwt', token, cookieOptions);
+
+    // Remove password from output
+    user.password = undefined;
 
     res.status(statusCode).json({
         status: 'success',
-        token
+        token,
+        data: {
+            user
+        }
     });
 }
 
 // Function to sign up a new user.
 const signup = catchAsync(async (req, res, next) => {
     const newUser = await User.create(req.body);
-
-    const token = signToken(newUser._id);
-
-    res.status(201).json({
-        status: 'success',
-        token,
-        data: {
-            user: newUser
-        }
-    });
+    createAndSendToken(newUser, 201, res);
 });
 
 // Function to login existing user.
