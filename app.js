0c7936dc4f188e17eccc5bd97a3da01174cf861b
visheshtomarr <visheshtomar7800@gmail.com>
2025-04-04 17:29:39 +0530

    Added some security measures

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
-const { max } = require('lodash');
 
 const app = express();
 
 // MIDDLEWARES
 // All the middlewares in the "Middleware stack" has access to the
 // 'req', 'res' and 'next' variable.
+app.use(helmet());
 
 if (process.env.NODE_ENV === 'development') {
     // Logger middleware
@@ -27,7 +30,13 @@ const limiter = rateLimit({
 app.use('/api', limiter);
 
 // Middleware to send data through requests.
-app.use(express.json());
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection.
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
 
 app.use((req, res, next) => {
     req.requestTime = new Date().toDateString();
