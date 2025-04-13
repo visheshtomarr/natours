1729269a813a4dbe517897ce9de9f15fb8b9755b
visheshtomarr <visheshtomar7800@gmail.com>
2025-04-13 18:52:05 +0530

    Improved read performance with indexes

--- models/tourModel.js
+++ models/tourModel.js
@@ -115,6 +115,10 @@ const tourSchema = new mongoose.Schema({
     toObject: { virtuals: true }
 });
 
+// Create index based on price.
+tourSchema.index({ price: 1, ratingsAverage: -1 });
+tourSchema.index({ slug: 1 });
+
 // Virtual property for our schema.
 tourSchema.virtual('durationWeeks').get(function () {
     return this.duration / 7;
