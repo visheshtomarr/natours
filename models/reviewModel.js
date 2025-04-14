386e6273a6d9ebe3d1f6c22bf552718f01bde513
visheshtomarr <visheshtomar7800@gmail.com>
2025-04-14 19:44:02 +0530

    Calculate average and no. of ratings on a tour

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./../models/tourModel');
 
 const reviewSchema = mongoose.Schema({
     review: {
@@ -37,6 +38,55 @@ reviewSchema.pre(/^find/, function (next) {
     });
 
     next();
+});
+
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+    // 'this' points to the current model.
+    const stats = await this.aggregate([
+        {
+            $match: { tour: tourId }
+        },
+        {
+            $group: {
+                // Grouping by tours.
+                _id: '$tour',
+                nRating: { $sum: 1 },
+                avgRating: { $avg: '$rating' }
+            }
+        }
+    ]);
+
+    if (stats.length > 0) {
+        await Tour.findByIdAndUpdate(tourId, {
+            ratingsQuantity: stats[0].nRating,
+            ratingsAverage: stats[0].avgRating
+        });
+    } else {
+        await Tour.findByIdAndUpdate(tourId, {
+            ratingsQuantity: 0,
+            ratingsAverage: 4.5
+        });
+    }
+}
+
+// We calculate the statistics after the data is persisted into our DB,
+// so that we can calculate the correct results.
+reviewSchema.post('save', function () {
+    // 'this.constructor' points to the current model.
+    this.constructor.calcAverageRatings(this.tour);
+});
+
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+    // We are creating a review property on the current query
+    // before the query is executed.
+    // 'this.r' will be the current review document containing the current tour id.
+    this.r = await this.findOne();
+    next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+    // 'this.r.constructor' will point to the current review model.
+    this.r.constructor.calcAverageRatings(this.r.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
