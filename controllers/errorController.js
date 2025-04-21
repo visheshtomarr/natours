692f4db719359ce621f4394c3d25133c25a52390
visheshtomarr <visheshtomar7800@gmail.com>
2025-04-22 00:28:50 +0530

    Implemented rendering of error pages

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -22,35 +22,64 @@ const handleJWTError = () => new AppError('Invalid token! Please login again.', 
 
 const handleJWTExpiredError = () => new AppError('Token has expired! Please login again.', 401)
 
-const sendErrorDev = (errorDev, res) => {
-    res.status(errorDev.statusCode).json({
-        status: errorDev.status,
-        error: errorDev,
-        message: errorDev.message,
-        stack: errorDev.stack
+const sendErrorDev = (errorDev, req, res) => {
+    // API
+    if (req.originalUrl.startsWith('/api')) {
+        return res.status(errorDev.statusCode).json({
+            status: errorDev.status,
+            error: errorDev,
+            message: errorDev.message,
+            stack: errorDev.stack
+        });
+    }
+    // For rendering website
+    console.error('Error: ', errorDev);
+    return res.status(errorDev.statusCode).render('error', {
+        title: 'Something went wrong!',
+        msg: errorDev.message
     });
 }
 
-const sendErrorProd = (errorProd, res) => {
-    // Operational error, It is trusted and message can be sent to client.
-    if (errorProd.isOperational) {
-        res.status(errorProd.statusCode).json({
-            status: errorProd.status,
-            message: errorProd.message
-        });
-    }
-    // Programming or any other unknown error, It cannot be trusted
-    // and we don't leak the error details to client.
-    else {
+
+const sendErrorProd = (errorProd, req, res) => {
+    // API
+    if (req.originalUrl.startsWith('/api')) {
+        // Operational error, It is trusted and message can be sent to client.
+        if (errorProd.isOperational) {
+            return res.status(errorProd.statusCode).json({
+                status: errorProd.status,
+                message: errorProd.message
+            });
+        }
+        // Programming or any other unknown error, It cannot be trusted
+        // and we don't leak the error details to client.
         // 1. Log to console.
-        // console.error('Error: ', errorProd);
+        console.error('Error: ', errorProd);
 
         // Send a generic error message.
-        res.status(500).json({
+        return res.status(500).json({
             status: errorProd.status,
             message: 'Something went wrong!'
         });
     }
+
+    // For rendering website
+    if (errorProd.isOperational) {
+        return res.status(errorProd.statusCode).render('error', {
+            title: 'Something went wrong!',
+            msg: errorProd.message
+        });
+    }
+    // Programming or any other unknown error, It cannot be trusted
+    // and we don't leak the error details to client.
+    // 1. Log to console.
+    console.error('Error: ', errorProd);
+
+    // Send a generic error message.
+    return res.status(500).render('error', {
+        title: 'Something went wrong!',
+        msg: 'Please try again later!'
+    });
 }
 
 const globalErrorHandler = (err, req, res, next) => {
@@ -60,7 +89,7 @@ const globalErrorHandler = (err, req, res, next) => {
     err.status = err.status || 'error';
 
     if (process.env.NODE_ENV === 'development') {
-        sendErrorDev(err, res);
+        sendErrorDev(err, req, res);
     }
     else if (process.env.NODE_ENV === 'production') {
         let errCopy = { ...err };
@@ -71,7 +100,7 @@ const globalErrorHandler = (err, req, res, next) => {
         if (errCopy.name === 'JsonWebTokenError') errCopy = handleJWTError();
         if (errCopy.name === 'TokenExpiredError') errCopy = handleJWTExpiredError();
 
-        sendErrorProd(errCopy, res);
+        sendErrorProd(errCopy, req, res);
     }
 }
 
--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,5 +1,6 @@
 const Tour = require('./../models/tourModel');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 
 
 const getOverview = catchAsync(async (req, res, next) => {
@@ -16,6 +17,10 @@ const getTour = catchAsync(async (req, res, next) => {
         path: 'reviews',
         fields: 'review rating user'
     });
+
+    if (!tour) {
+        return next(new AppError('No tour found!', 404));
+    }
 
     res.status(200).render('tour', {
         title: tour.name,
