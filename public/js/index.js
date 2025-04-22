660951663bc9e581f28fef10982ea3b105d76564
visheshtomarr <visheshtomar7800@gmail.com>
2025-04-22 23:21:26 +0530

    Update user password using UI

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,14 @@
 import "@babel/polyfill";
 import { login, logout } from "./login";
 import { displayMap } from "./mapbox";
-import { updateData } from "./updateSettings";
+import { updateSettings } from "./updateSettings";
 
 // DOM Elements
 const mapbox = document.getElementById('map');
-const loginForm = document.querySelector('.form');
+const loginForm = document.querySelector('.form--login');
 const logoutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
+const userPasswordForm = document.querySelector('.form-user-password');
 
 // Delegation
 if (mapbox) {
@@ -33,6 +34,23 @@ if (userDataForm) {
         e.preventDefault();
         const email = document.getElementById('email').value;
         const name = document.getElementById('name').value;
-        updateData(name, email);
+        updateSettings({ name, email }, 'data');
+    })
+}
+
+if (userPasswordForm) {
+    userPasswordForm.addEventListener('submit', async e => {
+        e.preventDefault();
+        document.querySelector('.btn--save-password').textContent = 'Updating...';
+
+        const passwordCurrent = document.getElementById('password-current').value;
+        const password = document.getElementById('password').value;
+        const passwordConfirm = document.getElementById('password-confirm').value;
+        await updateSettings({ passwordCurrent, password, passwordConfirm }, 'password');
+
+        document.querySelector('.btn--save-password').textContent = 'Save password';
+        document.getElementById('password-current').value = '';
+        document.getElementById('password').value = '';
+        document.getElementById('password-confirm').value = '';
     })
 }
