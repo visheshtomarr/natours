4a38497b4184113db83388f33cf83bb20c608a00
visheshtomarr <visheshtomar7800@gmail.com>
2025-04-27 19:57:16 +0530

    Refactored code

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -63,7 +63,7 @@ const webhookCheckout = (req, res, next) => {
     if (event.type === 'checkout.session.completed') {
         createBookingCheckout(event.data.object);
     }
-    res.status(200).json({ received: true });
+    res.status(200).json({ received: true, session: event.data.object });
 }
 
 const createBooking = createOne(Booking);
